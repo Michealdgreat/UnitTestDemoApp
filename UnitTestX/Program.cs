3289e438333ef8c2fae561dfce21c6b3b7166cb4
MICHEAL SHODAMOLA <micheal.shodamola@centria.fi>
2023-11-27 00:12:39 +0200

    Add project files.

---
+++ UnitTestX/Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+//Red(check for errors), Green(make sure they all pass the test), Refactor(figure out how you can make it better)
+public class DisplayMessage
+{
+    public string Greeting(string firstName, int hourOftheDay)
+    {
+        string output = "";
+        if (hourOftheDay < 12)
+        {
+            output = $"Good Morning {firstName}";
+        }
+        else if (hourOftheDay < 18)
+        {
+            output = $"Good Afternoon {firstName}";
+        }
+        else
+        {
+            output = $"Good Evening {firstName}";
+        }
+
+        return output;
+    }
+}
---
+++ UnitTestXProject/testProgram.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+// Arrange, Act, Assert
+//Expected and Actual
+ 
+namespace UnitTestXProject
+{
+    public class testProgram
+    {
+        [Fact]
+        public void GreetingShouldReturnGoodMorningMessage()
+        {
+            //Arrange
+            DisplayMessage messages = new DisplayMessage();
+            string expected = "Good Morning Micheal";
+
+            //Act
+            string actual = messages.Greeting("Micheal", 2);
+
+            //Assert
+            Xunit.Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void GreetingShouldReturnGoodAfternoonMessage()
+        {
+            //Arrange
+            DisplayMessage messages = new DisplayMessage();
+            string expected = "Good Afternoon Micheal";
+
+            //Act
+            string actual = messages.Greeting("Micheal", 17);
+
+            //Assert
+            Xunit.Assert.Equal(expected, actual);
+        }
+
+        [Theory]
+        [InlineData("Micheal", 0, "Good Morning Micheal")]
+        public void GreetingShouldreturnexpectedvalue(string firstName, int hourOftheDay, string expected)
+        {
+            //Arrange
+            DisplayMessage messages = new DisplayMessage();
+           // string expected = "Good Afternoon Micheal";
+
+            //Act
+            string actual = messages.Greeting(firstName, hourOftheDay);
+
+            //Assert
+            Xunit.Assert.Equal(expected, actual);
+        }
+    }
+}
